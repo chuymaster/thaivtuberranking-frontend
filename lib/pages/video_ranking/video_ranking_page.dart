75562243b6e77b6ae44c83beb73260b7cf931478
Chuy <chuymaster3@hotmail.com>
2021-10-17 13:10:11 +0900

    Hide bottom navigation bar when scroll down

--- lib/pages/video_ranking/video_ranking_page.dart
+++ lib/pages/video_ranking/video_ranking_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:thaivtuberranking/common/component/center_circular_progress_indicator.dart';
 import 'package:thaivtuberranking/common/component/custom_constraints.dart';
 import 'package:thaivtuberranking/common/component/empty_error_notification.dart';
@@ -16,9 +17,15 @@ import 'entity/video_ranking.dart';
 class VideoRankingPage extends StatefulWidget {
   final OriginType originType;
   final VideoRankingType rankingType;
+  final VoidCallback didScrollDown;
+  final VoidCallback didScrollUp;
 
   const VideoRankingPage(
-      {Key? key, required this.originType, required this.rankingType})
+      {Key? key,
+      required this.originType,
+      required this.rankingType,
+      required this.didScrollDown,
+      required this.didScrollUp})
       : super(key: key);
   @override
   _VideoRankingPageState createState() => _VideoRankingPageState();
@@ -28,13 +35,28 @@ class _VideoRankingPageState extends State<VideoRankingPage> {
   final _repository = VideoRankingRepository();
   List<VideoRanking> _ranking = [];
   var _isLoading = true;
+  ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     MyApp.analytics.sendAnalyticsEvent(AnalyticsEvent.page_loaded,
         {AnalyticsParameterName.page_name: AnalyticsPageName.video_ranking});
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        widget.didScrollDown();
+      } else {
+        widget.didScrollUp();
+      }
+    });
     loadData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 
   void loadData() async {
@@ -79,6 +101,7 @@ class _VideoRankingPageState extends State<VideoRankingPage> {
         body = EmptyErrorNotification();
       } else {
         body = VideoRankingList(
+          scrollController: _scrollController,
           videoRankingList: _getFilteredVideoList(),
           onTap: (item) {
             UrlLauncher.launchURL(item.getVideoUrl());
