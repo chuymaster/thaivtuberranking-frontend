483ce1686a2761bec74cb7751b20148b1280313f
Chuy <chuymaster3@hotmail.com>
2021-10-17 20:54:50 +0900

    Use provider and viewModel for live page

--- lib/pages/live/live_page.dart
+++ lib/pages/live/live_page.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:thaivtuberranking/common/component/center_circular_progress_indicator.dart';
-import 'package:thaivtuberranking/common/component/custom_constraints.dart';
-import 'package:thaivtuberranking/common/component/error_dialog.dart';
 import 'package:thaivtuberranking/main.dart';
 import 'package:thaivtuberranking/common/component/thai_text.dart';
 import 'package:thaivtuberranking/pages/channel/channel_page.dart';
 import 'package:thaivtuberranking/pages/home/entity/origin_type.dart';
 import 'package:thaivtuberranking/pages/live/component/live_video_listtile.dart';
 import 'package:thaivtuberranking/pages/live/entity/live_video.dart';
-import 'package:thaivtuberranking/pages/live/live_repository.dart';
+import 'package:thaivtuberranking/pages/live/live_view_model.dart';
 import 'package:thaivtuberranking/services/analytics.dart';
-import 'package:thaivtuberranking/services/result.dart';
 import 'package:thaivtuberranking/services/url_launcher.dart';
+import 'package:provider/provider.dart';
 
 class LivePage extends StatefulWidget {
   final OriginType originType;
@@ -31,9 +29,7 @@ class LivePage extends StatefulWidget {
 }
 
 class _LivePageState extends State<LivePage> {
-  final LiveRepository _repository = LiveRepository();
-  List<LiveVideo> _liveVideos = [];
-  bool _isLoading = true;
+  late LiveViewModel liveViewModel = LiveViewModel(widget.originType);
   ScrollController _scrollController = ScrollController();
 
   @override
@@ -50,7 +46,7 @@ class _LivePageState extends State<LivePage> {
         widget.didScrollUp();
       }
     });
-    loadData();
+    liveViewModel.getLiveVideos();
   }
 
   @override
@@ -59,58 +55,28 @@ class _LivePageState extends State<LivePage> {
     _scrollController.dispose();
   }
 
-  void loadData() async {
-    final result = await _repository.getLiveVideos();
-    if (result is SuccessState) {
-      setState(() {
-        _liveVideos = result.value;
-        _isLoading = false;
-      });
-    } else if (result is ErrorState) {
-      ErrorDialog.showErrorDialog(
-          'ไม่สามารถโหลดข้อมูลไลฟ์ได้\nโปรดลองใหม่ในภายหลัง',
-          result.msg,
-          context);
-      setState(() {
-        _isLoading = false;
-      });
-    } else {
-      setState(() {
-        _isLoading = true;
-      });
-    }
-  }
-
-  List<LiveVideo> _getFilteredLiveVideos() {
-    switch (widget.originType) {
-      case OriginType.OriginalOnly:
-        return _liveVideos
-            .where((element) => (!element.channelIsRebranded))
-            .toList();
-      case OriginType.All:
-        return _liveVideos;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    Widget body;
-    if (_isLoading) {
-      body = CenterCircularProgressIndicator();
-    } else {
-      List<LiveVideo> liveVideos = _getFilteredLiveVideos();
-      if (liveVideos.isEmpty) {
-        body = _buildEmptyWidget();
-      } else {
-        body = _buildLiveVideosWidget();
-      }
-    }
-    return Center(
-      child: Container(
-        child: body,
-        constraints: CustomConstraints.pageBoxConstraints,
-      ),
-    );
+    return ChangeNotifierProvider(
+        create: (context) => liveViewModel,
+        child: Consumer<LiveViewModel>(
+          builder: (context, liveViewModel, child) {
+            // if (liveViewModel.errorMessage != null) {
+            //   ErrorDialog.showErrorDialog(
+            //       'ไม่สามารถโหลดข้อมูลไลฟ์ได้\nโปรดลองใหม่ในภายหลัง',
+            //       liveViewModel.errorMessage,
+            //       context);
+            //   liveViewModel.clearErrorMessage();
+            // }
+            if (liveViewModel.isLoading) {
+              return CenterCircularProgressIndicator();
+            } else if (liveViewModel.getFilteredLiveVideos().isEmpty) {
+              return _buildEmptyWidget();
+            } else {
+              return _buildLiveVideosWidget();
+            }
+          },
+        ));
   }
 
   Widget _buildEmptyWidget() {
@@ -125,42 +91,39 @@ class _LivePageState extends State<LivePage> {
   }
 
   Widget _buildLiveVideosWidget() {
-    if (_liveVideos.isEmpty) {
-      return _buildEmptyWidget();
-    } else {
-      int itemCount = _liveVideos.length;
-      var listView = ListView.builder(
-        controller: _scrollController,
-        itemBuilder: (context, index) {
-          return Container(
-              child: Ink(
-                  color: _buildRowColor(index, _liveVideos[index]),
-                  child: LiveVideoListTile(
-                    item: _liveVideos[index],
-                    onTap: (liveVideo) {
-                      UrlLauncher.launchURL(liveVideo.getVideoUrl());
-                      MyApp.analytics.sendAnalyticsEvent(
-                          AnalyticsEvent.open_video_url,
-                          {'url': liveVideo.getVideoUrl()});
-                    },
-                    onTapChannelName: (liveVideo) {
-                      Navigator.pushNamed(context, ChannelPage.route,
-                          arguments: liveVideo.channelId);
-                      MyApp.analytics.sendAnalyticsEvent(
-                          AnalyticsEvent.view_detail, {
-                        'channel_id': liveVideo.channelId,
-                        'channel_name': liveVideo.channelTitle
-                      });
-                    },
-                  )));
-        },
-        itemCount: itemCount,
-      );
+    final filteredLiveVideos = liveViewModel.getFilteredLiveVideos();
+    int itemCount = filteredLiveVideos.length;
+    var listView = ListView.builder(
+      controller: _scrollController,
+      itemBuilder: (context, index) {
+        return Container(
+            child: Ink(
+                color: _buildRowColor(index, filteredLiveVideos[index]),
+                child: LiveVideoListTile(
+                  item: filteredLiveVideos[index],
+                  onTap: (liveVideo) {
+                    UrlLauncher.launchURL(liveVideo.getVideoUrl());
+                    MyApp.analytics.sendAnalyticsEvent(
+                        AnalyticsEvent.open_video_url,
+                        {'url': liveVideo.getVideoUrl()});
+                  },
+                  onTapChannelName: (liveVideo) {
+                    Navigator.pushNamed(context, ChannelPage.route,
+                        arguments: liveVideo.channelId);
+                    MyApp.analytics.sendAnalyticsEvent(
+                        AnalyticsEvent.view_detail, {
+                      'channel_id': liveVideo.channelId,
+                      'channel_name': liveVideo.channelTitle
+                    });
+                  },
+                )));
+      },
+      itemCount: itemCount,
+    );
 
-      return Container(
-        child: listView,
-      );
-    }
+    return Container(
+      child: listView,
+    );
   }
 
   Color? _buildRowColor(int index, LiveVideo liveVideo) {
---
+++ lib/pages/live/live_view_model.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:thaivtuberranking/pages/home/entity/origin_type.dart';
+import 'package:thaivtuberranking/pages/live/entity/live_video.dart';
+import 'package:thaivtuberranking/pages/live/live_repository.dart';
+import 'package:thaivtuberranking/services/result.dart';
+
+class LiveViewModel extends ChangeNotifier {
+  final OriginType originType;
+  final LiveRepository _repository = LiveRepository();
+
+  bool isLoading = false;
+  String? errorMessage;
+
+  List<LiveVideo> _liveVideos = [];
+
+  LiveViewModel(this.originType);
+
+  void getLiveVideos() async {
+    isLoading = true;
+    final result = await _repository.getLiveVideos();
+    isLoading = false;
+    if (result is SuccessState) {
+      _liveVideos = result.value;
+    } else if (result is ErrorState) {
+      errorMessage = result.msg;
+    }
+    notifyListeners();
+  }
+
+  List<LiveVideo> getFilteredLiveVideos() {
+    switch (originType) {
+      case OriginType.OriginalOnly:
+        return _liveVideos
+            .where((element) => (!element.channelIsRebranded))
+            .toList();
+      case OriginType.All:
+        return _liveVideos;
+    }
+  }
+
+  void clearErrorMessage() {
+    errorMessage = null;
+  }
+}
