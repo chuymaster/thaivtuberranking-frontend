0d1edcb4352e51e1556fa3fc86d3c2cd8b78fbc4
Chuy <chuymaster3@hotmail.com>
2022-01-01 11:11:57 +0900

    Refactor view and viewmodel

--- lib/pages/add/add_page.dart
+++ lib/pages/add/add_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:thaivtuberranking/common/component/center_circular_progress_indicator.dart';
-import 'package:thaivtuberranking/common/component/error_dialog.dart';
 import 'package:thaivtuberranking/common/component/thai_text.dart';
 import 'package:thaivtuberranking/common/strings.dart';
 import 'package:thaivtuberranking/pages/add/channel_registration_view_model.dart';
@@ -12,7 +11,6 @@ import 'package:thaivtuberranking/services/result.dart';
 import 'package:thaivtuberranking/services/url_launcher.dart';
 
 import '../../main.dart';
-import 'add_complete_page.dart';
 import 'component/description_box.dart';
 
 class AddPage extends StatefulWidget {
@@ -27,16 +25,8 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
-  // Form key for input https://flutter.dev/docs/cookbook/forms/validation
-  final _formKey = GlobalKey<FormState>();
-
-  final _textEditingController = TextEditingController();
-  final _viewModel = ChannelRegistrationViewModel();
-
-  bool _isSubmitButtonDisabled = true;
-  bool _isValidated = false;
-
-  var _currentOriginType = OriginType.OriginalOnly;
+  late final _viewModel =
+      ChannelRegistrationViewModel(widget.vTuberChannelIdList);
 
   @override
   void initState() {
@@ -45,21 +35,13 @@ class _AddPageState extends State<AddPage> {
     MyApp.analytics.sendAnalyticsEvent(AnalyticsEvent.page_loaded,
         {AnalyticsParameterName.page_name: AnalyticsPageName.add});
 
-    // else if (viewModel.viewState is ErrorState) {
-    //     var errorMessage = (viewModel.viewState as ErrorState).msg;
-    //     ErrorDialog.showErrorDialog(
-    //         AddErrorMessage.failedToSubmit, errorMessage, context);
-    //     // setState(() {
-    //     //   _isSubmitButtonDisabled = false;
-    //     // });
-    //   } else if (viewModel.viewState is SuccessState) {
-    //     // Navigator.pushNamedAndRemoveUntil(
-    //     //     context, AddCompletePage.route, (route) => false);
-    //     // setState(() {
-    //     //   _isSubmitButtonDisabled = false;
-    //     // });
-    //     return Container();
-    //   }
+    _viewModel.addListener(() {
+      if (_viewModel.viewState is ErrorState) {
+        _viewModel.showErrorDialog(context);
+      } else if (_viewModel.viewState is SuccessState) {
+        _viewModel.navigateToCompletePage(context);
+      }
+    });
   }
 
   @override
@@ -68,7 +50,7 @@ class _AddPageState extends State<AddPage> {
       create: (context) => _viewModel,
       child: Consumer<ChannelRegistrationViewModel>(
         builder: (context, viewModel, _) {
-          if (viewModel.viewState is LoadingState) {
+          if (viewModel.shouldShowLoadingIndicator) {
             return CenterCircularProgressIndicator();
           } else {
             return ListView(
@@ -104,13 +86,9 @@ class _AddPageState extends State<AddPage> {
           children: [
             Radio(
               value: OriginType.OriginalOnly,
-              groupValue: _currentOriginType,
+              groupValue: _viewModel.currentOriginType,
               onChanged: (OriginType? value) {
-                if (value != null) {
-                  setState(() {
-                    _currentOriginType = value;
-                  });
-                }
+                _viewModel.onOriginTypeChanged(value);
               },
             ),
             ThaiText(
@@ -123,13 +101,9 @@ class _AddPageState extends State<AddPage> {
           children: [
             Radio(
               value: OriginType.All,
-              groupValue: _currentOriginType,
+              groupValue: _viewModel.currentOriginType,
               onChanged: (OriginType? value) {
-                if (value != null) {
-                  setState(() {
-                    _currentOriginType = value;
-                  });
-                }
+                _viewModel.onOriginTypeChanged(value);
               },
             ),
             ThaiText(
@@ -142,73 +116,19 @@ class _AddPageState extends State<AddPage> {
     );
   }
 
-  void _validateInputText() {
-    final currentState = _formKey.currentState;
-    if (currentState != null) {
-      setState(() {
-        _isValidated = currentState.validate();
-        _isSubmitButtonDisabled = !currentState.validate();
-      });
-    }
-  }
-
   Widget get _channelRegistrationBox {
-    var channelIdLength = 24;
-    var textFormField = TextFormField(
-      maxLength: channelIdLength, // แชนแนล YouTube ยาว 24 ตัวอักษร
-      decoration: InputDecoration(hintText: "UCqhhWjpw23dWhJ5rRwCCrMA"),
-      onChanged: (text) => {_validateInputText()},
-      validator: (value) {
-        if (value != null) {
-          if (value.isEmpty) {
-            return 'โปรดกรอกแชนแนล ID';
-          } else if (!value.startsWith('UC')) {
-            return 'แชนแนล ID ต้องขึ้นต้นด้วย UC';
-          } else if (value.length != channelIdLength) {
-            return 'แชนแนล ID ต้องมีความยาว $channelIdLength ตัวอักษร';
-          } else if (widget.vTuberChannelIdList.contains(value)) {
-            return AddErrorMessage.alreadyAdded;
-          }
-        }
-        return null;
-      },
-      controller: _textEditingController,
-    );
-
-    var submitButton = ElevatedButton(
-      child: ThaiText(
-        text: "ส่งข้อมูล",
-        fontWeight: FontWeight.bold,
-        color: Colors.white,
-      ),
-      onPressed: _isSubmitButtonDisabled
-          ? null
-          : () {
-              setState(() {
-                _isSubmitButtonDisabled = true;
-              });
-              if (_formKey.currentState?.validate() ?? false) {
-                _registerChannel(_textEditingController.text);
-              } else {
-                setState(() {
-                  _isSubmitButtonDisabled = false;
-                });
-              }
-            },
-    );
-
     List<Widget> columnWidgets = [];
     columnWidgets.add(Padding(
       child: ThaiText(text: "โปรดกรอกแชนแนล ID (ขึ้นต้นด้วย UC)"),
       padding: EdgeInsets.fromLTRB(0, 16, 8, 8),
     ));
     columnWidgets.add(Padding(
-      child: textFormField,
+      child: _channelIdTextFormField,
       padding: EdgeInsets.fromLTRB(0, 8, 8, 8),
     ));
 
     // โชว์ลิงค์ไปที่แชนแนลยูทูปให้เช็คก่อนส่ง
-    if (_isValidated) {
+    if (_viewModel.isRegisterButtonEnabled) {
       TextStyle defaultStyle =
           TextStyle(color: Colors.black, fontWeight: FontWeight.bold);
       TextStyle defaultRedStyle =
@@ -223,20 +143,18 @@ class _AddPageState extends State<AddPage> {
       columnWidgets.add(_typeRadio);
       columnWidgets.add(Padding(padding: EdgeInsets.all(8)));
 
-      var channelUrl =
-          "https://youtube.com/channel/" + _textEditingController.text;
       columnWidgets.add(RichText(
           text: TextSpan(style: defaultRedStyle, children: [
         TextSpan(text: 'โปรดตรวจสอบแชนแนลอีกครั้งก่อนส่งข้อมูล\n'),
         TextSpan(
-            text: channelUrl,
+            text: _viewModel.inputChannelUrl,
             style: linkStyle,
             recognizer: TapGestureRecognizer()
               ..onTap = () {
                 MyApp.analytics.sendAnalyticsEvent(
                     AnalyticsEvent.tap_channel_url_before_submit_add_request,
-                    {'channel_id': _textEditingController.text});
-                UrlLauncher.launchURL(channelUrl);
+                    {'channel_id': _viewModel.inputChannelId});
+                UrlLauncher.launchURL(_viewModel.inputChannelUrl);
               }),
       ])));
 
@@ -244,12 +162,12 @@ class _AddPageState extends State<AddPage> {
     }
 
     columnWidgets
-        .add(Align(alignment: Alignment.centerRight, child: submitButton));
+        .add(Align(alignment: Alignment.centerRight, child: _submitButton));
     return Align(
         alignment: Alignment.center,
         child: Container(
           child: Form(
-              key: _formKey,
+              key: _viewModel.formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: columnWidgets,
@@ -258,26 +176,30 @@ class _AddPageState extends State<AddPage> {
         ));
   }
 
-  void _registerChannel(String id) async {
-    MyApp.analytics.sendAnalyticsEvent(
-        AnalyticsEvent.request_add_channel, {'channel_id': id});
-
-    var type = 'undefined';
-    switch (_currentOriginType) {
-      case OriginType.OriginalOnly:
-        type = 'original';
-        break;
-      case OriginType.All:
-        type = 'all';
-        break;
-    }
-
-    _viewModel.registerChannel(id, type);
+  Widget get _channelIdTextFormField {
+    return TextFormField(
+      maxLength: _viewModel.channelIdLength,
+      decoration: InputDecoration(hintText: "UCqhhWjpw23dWhJ5rRwCCrMA"),
+      onChanged: (text) => {_viewModel.validateInputText()},
+      validator: (value) => _viewModel.validateFormValue(value),
+      controller: _viewModel.textEditingController,
+    );
   }
-}
 
-class AddErrorMessage {
-  static const String alreadyAdded = "แชนแนลนี้อยู่ในฐานข้อมูลจัดอันดับแล้ว";
-  static const String failedToSubmit =
-      "เกิดปัญหาในการส่งข้อมูล กรุณาลองใหม่ในภายหลัง";
+  Widget get _submitButton {
+    return ElevatedButton(
+        child: ThaiText(
+          text: "ส่งข้อมูล",
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+        onPressed: _viewModel.isRegisterButtonEnabled
+            ? () {
+                MyApp.analytics.sendAnalyticsEvent(
+                    AnalyticsEvent.request_add_channel,
+                    {'channel_id': _viewModel.textEditingController.text});
+                _viewModel.registerChannel();
+              }
+            : null);
+  }
 }
--- lib/pages/add/channel_registration_view_model.dart
+++ lib/pages/add/channel_registration_view_model.dart
@@ -1,16 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:thaivtuberranking/common/component/error_dialog.dart';
 import 'package:thaivtuberranking/pages/add/add_repository.dart';
+import 'package:thaivtuberranking/pages/home/entity/origin_type.dart';
 import 'package:thaivtuberranking/services/result.dart';
+
+import 'add_complete_page.dart';
 
 class ChannelRegistrationViewModel extends ChangeNotifier {
   final _repository = AddRepository();
+  final List<String> _channelIdList;
+
+  // Form key for input https://flutter.dev/docs/cookbook/forms/validation
+  final formKey = GlobalKey<FormState>();
+  final textEditingController = TextEditingController();
+  final int channelIdLength = 24; // แชนแนล YouTube ยาว 24 ตัวอักษร
+  OriginType currentOriginType = OriginType.OriginalOnly;
+  bool _isInputValidated = false;
 
   Result viewState = Result.idle();
 
-  void registerChannel(String channelId, String type) async {
+  ChannelRegistrationViewModel(this._channelIdList);
+
+  String get inputChannelId {
+    return textEditingController.text;
+  }
+
+  String get inputChannelUrl {
+    return "https://youtube.com/channel/" + inputChannelId;
+  }
+
+  bool get isRegisterButtonEnabled {
+    return (viewState is IdleState || viewState is ErrorState) &&
+        _isInputValidated;
+  }
+
+  bool get shouldShowLoadingIndicator {
+    return (viewState is LoadingState || viewState is SuccessState);
+  }
+
+  void registerChannel() async {
+    var type = 'undefined';
+    switch (currentOriginType) {
+      case OriginType.OriginalOnly:
+        type = 'original';
+        break;
+      case OriginType.All:
+        type = 'all';
+        break;
+    }
+
     viewState = Result.loading();
     notifyListeners();
-    viewState = await _repository.sendAddChannelRequest(channelId, type);
+    viewState = await _repository.sendAddChannelRequest(inputChannelId, type);
     notifyListeners();
   }
+
+  void validateInputText() {
+    final currentState = formKey.currentState;
+    if (currentState != null) {
+      _isInputValidated = currentState.validate();
+      notifyListeners();
+    }
+  }
+
+  String? validateFormValue(String? value) {
+    if (value != null) {
+      if (value.isEmpty) {
+        return 'โปรดกรอกแชนแนล ID';
+      } else if (!value.startsWith('UC')) {
+        return 'แชนแนล ID ต้องขึ้นต้นด้วย UC';
+      } else if (value.length != channelIdLength) {
+        return 'แชนแนล ID ต้องมีความยาว $channelIdLength ตัวอักษร';
+      }
+    } else if (_channelIdList.contains(value)) {
+      return ChannelRegistrationErrorMessage.alreadyAdded;
+    }
+    return null;
+  }
+
+  void onOriginTypeChanged(OriginType? value) {
+    if (value != null) {
+      currentOriginType = value;
+      notifyListeners();
+    }
+  }
+
+  void navigateToCompletePage(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(
+        context, AddCompletePage.route, (route) => false);
+  }
+
+  void showErrorDialog(BuildContext context) {
+    String errorMessage = (viewState as ErrorState).msg;
+    ErrorDialog.showErrorDialog(
+        ChannelRegistrationErrorMessage.failedToSubmit, errorMessage, context);
+    viewState = Result.idle();
+    notifyListeners();
+  }
+}
+
+class ChannelRegistrationErrorMessage {
+  static const String alreadyAdded = "แชนแนลนี้อยู่ในฐานข้อมูลจัดอันดับแล้ว";
+  static const String failedToSubmit =
+      "เกิดปัญหาในการส่งข้อมูล กรุณาลองใหม่ในภายหลัง";
 }
