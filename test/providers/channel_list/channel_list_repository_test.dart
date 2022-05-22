abfb49efa3bade989ea85bd5462fac772298c5ea
Chuy <chuymaster3@hotmail.com>
2022-05-22 16:52:08 +0900

    Use channel data list API v2

--- test/providers/channel_list/channel_list_repository_test.dart
+++ test/providers/channel_list/channel_list_repository_test.dart
@@ -9,18 +9,17 @@ import '../../http_client.mocks.dart';
 
 void main() {
   group('getChannelList', () {
-    test('returns unique channel list if the http call completes successfully',
+    test('returns channel info list if the http call completes successfully',
         () async {
       final client = MockClient();
       final repository = ChannelListRepository(client);
 
       Uri url = Uri.parse(
-          "https://storage.googleapis.com/thaivtuberranking.appspot.com/channel_data/list.json");
+          "https://storage.googleapis.com/thaivtuberranking.appspot.com/v2/channel_data/list.json");
       when(client.get(url)).thenAnswer((_) async => http.Response('''
 {
    "result":[
-      [
-         {
+      {
             "channel_id":"UCAnKhwx493i5myddnJFF_gg",
             "title":"test",
             "description":"test",
@@ -35,30 +34,12 @@ void main() {
             "updated_at":1641006011156,
             "is_rebranded":false
          }
-      ],
-      [
-         {
-            "channel_id":"UCAnKhwx493i5myddnJFF_gg",
-            "title":"test",
-            "description":"test",
-            "thumbnail_icon_url":"https://yt3.ggpht.com/_omfQkzNb_u2x8ldqJ9IwoZlCCYEQQm-cfkfjfuqkSgKZIvmwF2i4G5CdN-zUQtuM8cfBqEo3w=s240-c-k-c0x00ffffff-no-rj",
-            "subscribers":23800,
-            "views":829124,
-            "comments":null,
-            "videos":156,
-            "uploads":"UUAnKhwx493i5myddnJFF_gg",
-            "published_at":"2020-11-11T12:58:49.610362Z",
-            "last_published_video_at":"2021-12-31T13:08:37Z",
-            "updated_at":1641006011156,
-            "is_rebranded":false
-         }
-      ]
    ]
 }
             ''', 200));
 
       final result = await repository.getChannelList();
-      expect(result, isA<SuccessState<dynamic>>());
+      expect(result, isA<SuccessState<List<ChannelInfo>>>());
 
       final List<ChannelInfo> channelList = (result as SuccessState).value;
       expect(channelList.length, 1);
@@ -69,7 +50,7 @@ void main() {
       final repository = ChannelListRepository(client);
 
       Uri url = Uri.parse(
-          "https://storage.googleapis.com/thaivtuberranking.appspot.com/channel_data/list.json");
+          "https://storage.googleapis.com/thaivtuberranking.appspot.com/v2/channel_data/list.json");
       when(client.get(url)).thenAnswer((_) async => http.Response('', 500));
 
       final result = await repository.getChannelList();
