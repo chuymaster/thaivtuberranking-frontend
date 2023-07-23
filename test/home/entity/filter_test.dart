ca8a86442768a7847c7e1c8f4301ed1fba234176
Chuy <chatchai.lokniyom@gree.net>
2023-07-23 13:00:02 +0900

    Fix tests

--- test/home/entity/filter_test.dart
+++ test/home/entity/filter_test.dart
@@ -4,9 +4,9 @@ import 'package:thaivtuberranking/pages/home/entity/filter.dart';
 void main() {
   group('Filter enum tests', () {
     test('toString', () {
-      expect(Filter.Subscriber.toString(), "เรียงตามจำนวนผู้ติดตาม");
-      expect(Filter.View.toString(), "เรียงตามจำนวนการดู");
-      expect(Filter.PublishedDate.toString(), "เรียงตามวันเปิดแชนแนลล่าสุด");
+      expect(Filter.Subscriber.toString(), "ผู้ติดตาม");
+      expect(Filter.View.toString(), "Views");
+      expect(Filter.PublishedDate.toString(), "วันเปิดแชนแนล");
       expect(() => Filter.values[3], throwsA(isA<RangeError>()));
     });
   });
